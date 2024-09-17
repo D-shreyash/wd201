73bd9c65a9e7212e6c53b363cce2af569926e115
D-shreyash <shreyashdesai0519@gmail.com>
2024-09-17 12:05:51 +0530

    final commit

--- http-server/index.js
+++ http-server/index.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const http = require("http");
+const { register } = require("module");
 
 let home = "";
 let project = "";
+let registrationContent = "";
 
 fs.readFile("home.html", (err, data) => {
   if (err) throw err;
@@ -14,12 +16,23 @@ fs.readFile("project.html", (err, data) => {
   project = data;
 });
 
+fs.readFile("registration.html", (err, register) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = register;
+});
+
 const server = http.createServer((req, res) => {
   let url = req.url;
   res.writeHeader(200, { "content-type": "text/html" });
   switch (url) {
     case "/project":
       res.write(project);
+      res.end();
+      break;
+    case "/registration":
+      res.write(registrationContent);
       res.end();
       break;
     default:
