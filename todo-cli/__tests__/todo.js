1f0955f7acc6b815a72828029c371c36bb6d0287
D-shreyash <shreyashdesai0519@gmail.com>
2024-09-26 23:12:24 +0530

    Add pre-commit hook to run tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,19 +1,23 @@
 const { todoList, formattedDate } = require("../todo.js"); // Adjust the path as needed
 
+// eslint-disable-next-line no-undef
 describe("Todo List Tests", () => {
   let todos; // Variable to hold the todo list instance
 
   // Initialize a new todo list before each test
+  // eslint-disable-next-line no-undef
   beforeEach(() => {
     todos = todoList(); // Create a new todo list instance
   });
 
+  // eslint-disable-next-line no-undef
   test("Should add a todo item", () => {
     todos.add({
       title: "Pay electric bill",
       dueDate: formattedDate(new Date()),
       completed: false,
     });
+    // eslint-disable-next-line no-undef
     expect(todos.all[0].title).toBe("Pay electric bill");
   });
 
